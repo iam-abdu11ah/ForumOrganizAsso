b2af758fbfbecfe56758120ef94ff4498be9b4d3
Abdullah Al Mamun <maamun101@gmail.com>
2026-06-17 23:16:40 +0200

    changed backend url origin to deployed url

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(session({
     }
 }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://organiz-asso.vercel.app/',
     credentials: true
 }));
 app.use(express.json());
